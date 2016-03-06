fbd819c6071e4cdeba50a0f7351aa8f06d37b566
Courtney Wilburn <courtney@cjwilburn.com>
2016-03-06 16:53:23 -0500

    adding model for resisting arrest data

--- app/models/response-resistance.js
+++ app/models/response-resistance.js
@@ -7,9 +7,121 @@ var mongoosePaginate = require('mongoose-paginate');
 exports = module.exports = function(mongoose, iglooMongoosePlugin) {
 
   var ResponseResistance = new mongoose.Schema({
-    name: {
-      type: String,
+    RIN: {
+      type: Number,
+      required: false
+    },
+    PrimaryKey: {
+      type: Number,
       required: true
+    },
+    DateOccurred: {
+      type: Date,
+      required: true
+    },
+    TimeOccurred: {
+      type: Date,
+      required: true
+    },
+    Location: {
+      type: Text,
+      required: true
+    },
+    AreaCommand: {
+      type: Text,
+      required: false
+    },
+    NatureOfContact: {
+      type: Text,
+      required: false
+    },
+    ReasonDesc: {
+      type: Text,
+      required: false
+    },
+    R2RLevel: {
+      type: Text,
+      required: false
+    },
+    MasterSubjectID: {
+      type: Number,
+      required: false
+    },
+    SubjectSex: {
+      type: Text,
+      required: false
+    },
+    SubjectRace: {
+      type: Text,
+      required: false
+    },
+    SubjectEthnicity: {
+      type: Text,
+      required: false
+    },
+    SubjectRole: {
+      type: Text,
+      required: false
+    },
+    SubjectConductDesc: {
+      type: Text,
+      required: false
+    },
+    SubjectResistance: {
+      type: Text,
+      required: false
+    },
+    WeaponUsed1: {
+      type: Text,
+      required: false
+    },
+    WeaponUsed2: {
+      type: Text,
+      required: false
+    },
+    WeaponUsed3: {
+      type: Text,
+      required: false
+    },
+    WeaponUsed4: {
+      type: Text,
+      required: false
+    },
+    WeaponUsed5: {
+      type: Text,
+      required: false
+    },
+    NumberShots: {
+      type: Text,
+      required: false
+    },
+    SubjectEffects: {
+      type: Text,
+      required: false
+    },
+    EffectOnOfficer: {
+      type: Text,
+      required: false
+    },
+    OfficerOrganizationDesc: {
+      type: Text,
+      required: false
+    },
+    OfficerCommissionDate: {
+      type: Date,
+      required: false
+    },
+    OfficerYrsOfService: {
+      type: Number,
+      required: false
+    },
+    X_Coordinate: {
+      type: Number,
+      required: false
+    },
+    Y_Coordinate: {
+      type: Number,
+      required: false
     }
   });
 
